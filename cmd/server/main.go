f02832403e65ce828b76841a58534c0a8274b11f
Cameron-Guinnee <cmguinnee@gmail.com>
2026-06-14 19:02:38 -0500

    Wired the store into cmd/server/main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,17 +2,26 @@ package main
 
 import (
     "fmt"
+    "log"
     "net/http"
 
     "github.com/go-chi/chi/v5"
+    "github.com/YOUR_USERNAME/dayplan/internal/store"
 )
 
 func main() {
+    db, err := store.New("dayplan.db")
+    if err != nil {
+        log.Fatalf("failed to open database: %v", err)
+    }
+
     r := chi.NewRouter()
 
     r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintln(w, "dayplan is running")
     })
+
+    _ = db // we'll use this shortly
 
     fmt.Println("Server starting on :8080")
     http.ListenAndServe(":8080", r)
